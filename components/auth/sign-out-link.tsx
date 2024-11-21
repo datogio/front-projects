5fb4d18d3c8f3b711260b11b8ee19c236258b501
David Giorgadze <datogio@gmail.com>
2024-11-22 00:08:50 +0400

    Authentication (#8)
    * feat: introduce authentication pages
    
    * feat: implement layouts
    
    * style: center auth layout
    
    * feat: introduce sign in form
    
    * feat: add dummy links to the form
    
    * feat: add names to inputs
    
    * feat: prepare markup for auth pages
    
    * style: rename fuction & remove unused import
    
    * feat: implement sign in form controll
    
    * feat: implement sign in action
    
    * feat: introduce API_URL env variable
    
    * style: add comment
    
    * feat: implement middleware for redirections
    
    * feat: implement sign up
    
    * feat: implement validations
    
    * feat: implement sign out
    
    * feat: implement sign out component
    
    * style: rename component
    
    ---------
    
    Co-authored-by: David Giorgadze <fitportal.mail@gmail.com>

---
+++ components/auth/sign-out-link.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { signOut } from "@/actions";
+import { useRouter } from "next/navigation";
+
+export function SignOutLink() {
+  const router = useRouter();
+
+  const handleSignOut = () => {
+    const confirmSignOut = confirm("Are you sure?");
+
+    confirmSignOut &&
+      signOut()
+        .then(() => {
+          router.push("/auth/sign-in");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+  };
+
+  return (
+    <div
+      className="cursor-pointer transition hover:text-gray-400"
+      onClick={handleSignOut}
+    >
+      Sign Out
+    </div>
+  );
+}
