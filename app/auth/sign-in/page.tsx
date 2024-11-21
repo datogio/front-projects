743bc484a873d91c7d1d1d38c6b1e31b9d99acd0
David Giorgadze <fitportal.mail@gmail.com>
2024-11-21 20:28:48 +0400

    feat: introduce sign in form

--- app/auth/sign-in/page.tsx
+++ app/auth/sign-in/page.tsx
@@ -1,3 +1,22 @@
 export default function SignInPage() {
-  return <div>Sign In</div>;
+  return (
+    <div className="bg-gray-800 w-[30vw] flex flex-col items-center p-10 rounded-xl gap-10">
+      <h1 className="text-5xl">Sign In</h1>
+      <form className="w-full flex flex-col gap-5">
+        <input
+          className="p-2 rounded-lg text-lg text-black outline-none"
+          type="email"
+          placeholder="Enter your email"
+        />
+        <input
+          className="p-2 rounded-lg text-lg text-black outline-none"
+          type="password"
+          placeholder="Enter your password"
+        />
+        <button className="bg-black p-3 rounded-lg text-lg font-bold transition hover:scale-105">
+          Sign In
+        </button>
+      </form>
+    </div>
+  );
 }
