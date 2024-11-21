f2310d26bd0ff06451fc0ab6213c8dd4f470523c
David Giorgadze <fitportal.mail@gmail.com>
2024-11-21 23:27:14 +0400

    feat: implement sign up

---
+++ actions/auth/sign-up.ts
@@ -0,0 +1,29 @@
+"use server";
+
+import axios, { AxiosRequestConfig } from "axios";
+import { cookies } from "next/headers";
+
+export async function signUp(
+  email: string,
+  password: string
+): Promise<{ userId: string; email: string }> {
+  const config: AxiosRequestConfig = {
+    method: "POST",
+    url: `http://${process.env.API_URL}/api/auth/sign-up`,
+    data: { email, password },
+  };
+
+  try {
+    const response = await axios(config);
+
+    const cookie = await cookies();
+    cookie.set({
+      name: "token",
+      value: response.data.token,
+      httpOnly: true,
+    });
+    return { userId: response.data.userId, email: response.data.email };
+  } catch (err: any) {
+    throw new Error(err);
+  }
+}
--- app/auth/sign-up/page.tsx
+++ app/auth/sign-up/page.tsx
@@ -1,33 +1,86 @@
+"use client";
+
+import { signUp } from "@/actions";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { ChangeEventHandler, FormEventHandler, useState } from "react";
 
 export default function SignUpPage() {
+  const router = useRouter();
+  const [inputs, setInputs] = useState({
+    email: "",
+    confirmEmail: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleFormSubmit: FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
+
+    if (!inputs.email || !inputs.password) {
+      return alert("Both fields are required");
+    }
+
+    signUp(inputs.email, inputs.password)
+      .then(() => {
+        router.push("/");
+      })
+      .catch(() => {
+        alert("Invalid email or password");
+      })
+      .finally(() => {
+        setInputs({
+          email: "",
+          confirmEmail: "",
+          password: "",
+          confirmPassword: "",
+        });
+      });
+  };
+
+  const handleInputChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+    setInputs((prev) => ({ ...prev, [event.target.name]: event.target.value }));
+  };
+
   return (
     <div className="bg-gray-800 w-[30vw] flex flex-col items-center p-10 rounded-xl gap-10">
       <h1 className="text-5xl">Sign Up</h1>
-      <form className="w-full flex flex-col gap-5">
+      <form className="w-full flex flex-col gap-5" onSubmit={handleFormSubmit}>
         <input
           className="p-2 rounded-lg text-lg text-black outline-none"
+          autoComplete="off"
           type="email"
           name="email"
           placeholder="Enter your email"
+          value={inputs.email}
+          onChange={handleInputChange}
         />
         <input
           className="p-2 rounded-lg text-lg text-black outline-none"
+          autoComplete="off"
           type="email"
           name="confirmEmail"
           placeholder="Enter your email"
+          value={inputs.confirmEmail}
+          onChange={handleInputChange}
         />
         <input
           className="p-2 rounded-lg text-lg text-black outline-none"
+          autoComplete="off"
           type="password"
           name="password"
           placeholder="Enter your password"
+          value={inputs.password}
+          onChange={handleInputChange}
         />
         <input
           className="p-2 rounded-lg text-lg text-black outline-none"
+          autoComplete="off"
           type="password"
           name="confirmPassword"
           placeholder="Enter your password"
+          value={inputs.confirmPassword}
+          onChange={handleInputChange}
         />
         <button className="bg-black p-3 rounded-lg text-lg font-bold transition hover:scale-105">
           Sign Up
